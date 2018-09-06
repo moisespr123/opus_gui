4a2a294f708713185e83d7e9e22ea1fa07947116
moisesmcardona <cardona_76937@students.pupr.edu>
2018-09-06 14:59:57 -0400

    Multithreaded encoding

--- opus_gui/Form1.Designer.vb
+++ opus_gui/Form1.Designer.vb
@@ -42,9 +42,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(228, 13)
+        Me.Label1.Size = New System.Drawing.Size(327, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Step 1: Browse for Input folder with FLAC Files:"
+        Me.Label1.Text = "Step 1: Browse for Input folder with opusenc-compatible music Files:"
         '
         'Label2
         '
--- opus_gui/Form1.vb
+++ opus_gui/Form1.vb
@@ -21,18 +21,53 @@
     End Sub
 
     Private Sub StartBtn_Click(sender As Object, e As EventArgs) Handles StartBtn.Click
+        StartBtn.Enabled = False
+        InputTxt.Enabled = False
+        OutputTxt.Enabled = False
+        InputBrowseBtn.Enabled = False
+        OutputBrowseBtn.Enabled = False
+        BitrateTextBox.Enabled = False
+        Dim StartTasks As New Threading.Thread(Sub() StartThreads())
+        StartTasks.Start()
+    End Sub
+    Private Sub StartThreads()
         If Not My.Computer.FileSystem.DirectoryExists(OutputTxt.Text) Then My.Computer.FileSystem.CreateDirectory(OutputTxt.Text)
-        ProgressBar1.Maximum = My.Computer.FileSystem.GetFiles(InputTxt.Text).Count
-        ProgressBar1.Value = 0
+        Dim ItemsToProcess As List(Of String) = New List(Of String)
         For Each File In IO.Directory.GetFiles(InputTxt.Text)
-            Dim FileWithoutExt As String = IO.Path.GetFileNameWithoutExtension(File)
-            Run_opus(File, OutputTxt.Text + "\" + FileWithoutExt + ".opus", BitrateTextBox.Text)
-            ProgressBar1.PerformStep()
-            Me.Update()
+            If IO.Path.GetExtension(File) = ".wav" Or IO.Path.GetExtension(File) = ".flac" Or IO.Path.GetExtension(File) = ".opus" Then
+                ItemsToProcess.Add(File)
+            Else
+                If Not My.Computer.FileSystem.FileExists(OutputTxt.Text + "\" + My.Computer.FileSystem.GetName(File)) Then
+                    My.Computer.FileSystem.CopyFile(File, OutputTxt.Text + "\" + My.Computer.FileSystem.GetName(File))
+                End If
+            End If
         Next
+        ProgressBar1.BeginInvoke(Sub()
+                                     ProgressBar1.Maximum = ItemsToProcess.Count
+                                     ProgressBar1.Value = 0
+                                 End Sub
+        )
+        Dim tasks = New Task(ItemsToProcess.Count - 1) {}
+        For Counter As Integer = 0 To ItemsToProcess.Count - 1
+            Dim args As Array = {ItemsToProcess(Counter), OutputTxt.Text + "\" + IO.Path.GetFileNameWithoutExtension(ItemsToProcess(Counter)) + ".opus", My.Settings.Bitrate}
+            tasks(Counter) = Task.Factory.StartNew(Function() Run_opus(args))
+
+        Next
+        Task.WaitAll(tasks)
+        StartBtn.BeginInvoke(Sub()
+                                 StartBtn.Enabled = True
+                                 BitrateTextBox.Enabled = True
+                                 InputTxt.Enabled = True
+                                 OutputTxt.Enabled = True
+                                 InputBrowseBtn.Enabled = True
+                                 OutputBrowseBtn.Enabled = True
+                             End Sub)
         MsgBox("Finished")
     End Sub
-    Private Sub Run_opus(Input_File As String, Output_File As String, Bitrate As String)
+    Private Function Run_opus(args As Array)
+        Dim Input_File As String = args(0)
+        Dim Output_File As String = args(1)
+        Dim Bitrate As String = args(2)
         Dim flacProcessInfo As New ProcessStartInfo
         Dim flacProcess As Process
         flacProcessInfo.FileName = "opusenc.exe"
@@ -42,7 +77,8 @@
         flacProcessInfo.UseShellExecute = False
         flacProcess = Process.Start(flacProcessInfo)
         flacProcess.WaitForExit()
-    End Sub
+        Return True
+    End Function
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         BitrateTextBox.Text = My.Settings.Bitrate
