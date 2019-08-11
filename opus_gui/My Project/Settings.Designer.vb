4227bb9f63ecb89d96fd248e3e1eb5d369a1030a
moisesmcardona <moises@moises-studios.com>
2019-08-11 12:34:41 -0400

    Removed unused option.

--- opus_gui/My Project/Settings.Designer.vb
+++ opus_gui/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -111,18 +111,6 @@ Namespace My
             End Get
             Set
                 Me("EncFfmpeg2") = value
-            End Set
-        End Property
-        
-        <Global.System.Configuration.UserScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("4")>  _
-        Public Property MaxDriveDownloads() As Integer
-            Get
-                Return CType(Me("MaxDriveDownloads"),Integer)
-            End Get
-            Set
-                Me("MaxDriveDownloads") = value
             End Set
         End Property
     End Class
